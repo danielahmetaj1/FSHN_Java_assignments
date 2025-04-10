015fc0dc3b83815d4508d8670e9f96c63b2c0bcb
danielahmetaj1 <danielahmetaj22@gmail.com>
2025-04-10 18:48:24 +0200

    Ushtrimi 3, Puna 4

---
+++ Laboratory_assignments/Laborator2/Puna4/src/Telefon_ush3.java
@@ -0,0 +1,46 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+
+public class Telefon_ush3 extends JFrame {
+    private final JTextField textField;
+    private final StringBuilder number;
+
+    public Telefon_ush3() {
+        setTitle("Telefon");
+        setSize(300, 300);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new BorderLayout());
+        setLocationRelativeTo(null);
+        JPanel buttonPanel = new JPanel();
+        number = new StringBuilder();
+        textField = new JTextField(12);
+        textField.setEditable(false);
+
+        buttonPanel.setLayout(new GridLayout(4, 3));
+
+        String[] buttons = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "*", "0", "#"};
+
+        for (String btnText : buttons) {
+            JButton button = new JButton(btnText);
+//            button.setFont(new Font("Arial", Font.BOLD, 20));
+            button.addActionListener((ActionEvent e) -> {
+                JButton source = (JButton) e.getSource();
+                number.append(source.getText());
+                textField.setText(number.toString());
+            });
+            buttonPanel.add(button);
+        }
+
+        add(buttonPanel, BorderLayout.CENTER);
+        JPanel bottomPanel = new JPanel();
+        bottomPanel.add(new JLabel("Numri:"));
+        bottomPanel.add(textField);
+        add(bottomPanel, BorderLayout.SOUTH);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new Telefon_ush3();
+    }
+}
