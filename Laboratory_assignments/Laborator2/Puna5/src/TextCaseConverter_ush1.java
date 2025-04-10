1ffc81cde9d0652781fae621acdc1788ff57b677
danielahmetaj1 <danielahmetaj22@gmail.com>
2025-04-10 20:56:31 +0200

    Ush 1 puna 5

--- Laboratory_assignments/Laborator2/Puna5/src/TextCaseConverter_ush1.java
+++ Laboratory_assignments/Laborator2/Puna5/src/TextCaseConverter_ush1.java
@@ -1,7 +1,7 @@
 /*1. Ndërtoni një aplikacion i cili do të përmbajë një frame, me një textArea ku do te
 afishohet një tekst. Të shtohen 2 butona: “To Lower Case”, dhe “To Upper Case”
 që do te kthejnë tekstin në shkronja të vogla ose të mëdha në varësi të butonit të
-zgjedhur.*/
+zgjedhur. */
 
 import javax.swing.*;
 import java.awt.*;
